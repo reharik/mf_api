59930182ff5a3e8ac7a4a37d18fcf17d08814983
reharik <reharik@gmail.com>
2015-06-06 16:44:37 -0500

    gulp file

--- src/config/gulp.js
+++ src/config/gulp.js
@@ -5,11 +5,11 @@ module.exports = {
   paths: {
       "in": {
           srcfiles: [root + "/src/**/*.js", root + "/src/package.json"],
-          dockerfiles:[root + "/docker-shell.sh",root + "/Dockerfile", root + "/Makefile",]
+          dockerfiles:[root + "/docker-shell.sh",root + "/Dockerfile", root + "/Makefile"]
       },
       out: {
           root: root + "/../MF_BuildFiles/Api",
-          src: root + "/../MF_BuildFiles/Api/src",
+          src: root + "/../MF_BuildFiles/Api/src"
       }
   }
 };
