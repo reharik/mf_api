263fdfd158124ab9fac4ae0e28242079e602d978
Raif Harik <reharik@gmail.com>
2015-06-08 14:35:45 -0500

    adding port to ips

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   mongo: {
-    url: 'mongodb://mongo/methodfitness-dev'
+    url: 'mongodb://:27017/methodfitness-dev'
   },
   eventstore:{
     ip:'eventstore'
   },
   cdn:{
-    ip:'cdn'
+    ip:'cdn:9090'
   },
   app: {
     title: 'MethodFitness_JS - Development Environment'
